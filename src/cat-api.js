95a29e0ba9868f21941852e64a5b4d83997c497a
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-05-31 06:50:23 +0300

    финиш

--- src/cat-api.js
+++ src/cat-api.js
@@ -9,42 +9,34 @@ import {
   pLoader,
   pError,
 } from './index';
-export { fetchBreeds, fetchBreedById};
+export { fetchBreeds, fetchBreedById };
 
-    
 const selectBreed = document.querySelector('.breedselect');
 const pLoader = document.querySelector('.loader');
 
 const api_key =
   'live_GmEyPinTRF7Q7LURfrAEdeqCEigZuDD4dcT4ZAQbZLhtFQ0FHyICoGRmHHGSy1P0';
 
-  
-
-
 function fetchBreeds() {
-    Notiflix.Loading.standard(`${pLoader.textContent}`);
-    
-    
-  const urlBreeds = `https://api.thecatapi.com/v1/breeds`; 
+  Notiflix.Loading.standard(`${pLoader.textContent}`);
+
+  const urlBreeds = `https://api.thecatapi.com/v1/breeds`;
   return fetch(urlBreeds, {
     headers: {
       'x-api-key': api_key,
     },
-  })
-    
+  });
 }
 // console.log(breedId);
 
-
 function fetchBreedById() {
-    catInfoBox.classList.add('hide');
-    loaderShow();
+  catInfoBox.classList.add('hide');
+  loaderShow();
   const urlSearchBreeds = `https://api.thecatapi.com/v1/images/search?api_key=${api_key}&breed_ids=${breedId}`;
 
   return fetch(urlSearchBreeds, {
     headers: {
       'x-api-key': api_key,
     },
-  })
-    
+  });
 }
