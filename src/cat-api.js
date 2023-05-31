38a8ceabef7b5d1b28dd9846979de91969a79326
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-05-31 06:58:05 +0300

    12354

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,4 @@
-import SlimSelect from 'slim-select';
+
 import Notiflix from 'notiflix';
 import {
   loaderHide,
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import SlimSelect from 'slim-select';
+
 import Notiflix from 'notiflix';
 
 import { fetchBreeds, fetchBreedById } from './cat-api.js';
