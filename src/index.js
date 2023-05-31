e3db4c5c2f9df36820dbcbc0016e0eb41b86f955
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-05-31 06:43:25 +0300

    555

--- src/index.js
+++ src/index.js
@@ -1,101 +1,130 @@
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
 
-import { fetchBreeds, fetchBreedById, selectBreed } from './cat-api.js';
+import { fetchBreeds, fetchBreedById } from './cat-api.js';
 
-
-let storedBreed = [];
+export {
+  loaderHide,
+  loaderShow,
+  storedBreed,
+  breedId,
+  catInfoBox,
+  pLoader,
+  pError,
+};
+  
+const selectBreed = document.querySelector('.breedselect');
+const pLoader = document.querySelector('.loader');
+const pError = document.querySelector('.error');
+const catInfoBox = document.querySelector('.cat-info');
 let breedId = [];
+let storedBreed = [];
+
 
 fetchBreeds()
   .then(response => {
-    if (response) {
+    if (response.ok) {
       return response.json();
     }
   })
+
+  .then(data => {
+    allBreedSelect(data);
+    Notiflix.Loading.remove();
+    selectBreed.classList.remove('hide');
+  })
+
   .catch(function (error) {
+    console.log('erreo');
     pError.classList.remove('hide');
     Notiflix.Report.failure(error);
-    loaderHide();
   });
 
 
 
-// fetchBreedById()
-//   .then(response => {
-//     if (response) {
-//       return response.json();
-//     }
-//   })
-//   .then(data => {})
-//   .catch(function (error) {
-//     pError.classList.remove('hide');
-//     Notiflix.Report.failure(error);
-//   });
-  
+function allBreedSelect(data) {
+  storedBreed = data;
+  for (let i = 0; i < storedBreed.length; i++) {
+    const breed = storedBreed[i];
+    // console.log(storedBreed[i]);
+    let option = document.createElement('option');
+
+    option.value = breed.id;
+
+    option.innerHTML = `${breed.name}`;
+    selectBreed.appendChild(option);
+  }
+}
 
 
 
+const fetchCatByBreed = event => {
+  breedId = event.currentTarget.value;
+  console.log(breedId);
+  fetchBreedById()
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(data => {
+      getCatInfo(data);
+      Notiflix.Loading.remove(500);
+      loaderHide();
+      catInfoBox.classList.remove('hide');
+    })
+    .catch(function (error) {
+      pError.classList.remove('hide');
+      Notiflix.Report.failure(error);
+    });
+};
+
+selectBreed.addEventListener('change', fetchCatByBreed);
 
 
 
+function getCatInfo(data) {
+  const breedinfo = data[0].breeds[0];
+  document.querySelector('.cat-name').textContent = breedinfo.name;
+  document.querySelector(
+    '.cat-description'
+  ).textContent = `Description: ${breedinfo.description}`;
+  document.querySelector(
+    '.cat-temperament'
+  ).textContent = `Temperament: ${breedinfo.temperament}`;
 
+  document.getElementById('breed_image').src = data[0].url;
+  document.getElementById('breed_image').alt = breedinfo.id;
+  console.log(breedinfo.id);
+}
 
+function catBox() {
+  const box = `<div class="cat_text_info">
+<h2 class ="cat-name"></h2>
+<p class = "cat-description"></p>
+<ul class="temp_list">
+<li class="cat-temperament"></li>
+</ul>
+</div>
 
+<div>
+<img id="breed_image" width="390"></img>
+</div>`;
+  catInfoBox.insertAdjacentHTML('afterbegin', box);
+}
 
+catBox();
 
+function loaderHide() {
+  if (!pLoader.classList.contains('hide')) {
+    pLoader.classList.add('hide');
+  }
+  return;
+}
 
-
-
-// const select = document.querySelector('.breed-select');
-// const cotInfo = document.querySelector('.cat-info');
-
-// select.addEventListener('change', functionSelect);
-
-// fetchBreeds()
-//   .then(data => {
-//     const markup = createMarkup(data);
-//     select.innerHTML = markup;
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-
-// function functionSelect() {
-//   const selectedOptionValue = select.value;
-//   if (selectedOptionValue) {
-//     fetchBreeds(select.value);
-//   }
-// }
-
-// function createMarkup(arr) {
-//   return arr
-//     .map(
-//       breed => `<option value="${breed.id}">${breed.name}</option>
-//     `
-//     )
-//     .join('');
-// }
-
-// function createMarkupInfo(arr) {
-//   console.log(arr);
-//   return arr
-//     .map(
-//       breed => `<ul>
-//       <img src="${breed.url}" alt="${breed.name}">
-//       <li>${breed.id}</li>
-//       <li>${breed.description}</li>
-//       <li>${breed.temperament}</li>
-//     </ul>`
-//     )
-//     .join('');
-// }
-
-// fetchCatByBreed()
-//   .then(data => {
-//     const markupInfo = createMarkupInfo(data);
-//     console.log(data);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
+function loaderShow() {
+  if (pLoader.classList.contains('hide')) {
+    pLoader.classList.remove('hide');
+  }
+  return;
+}
